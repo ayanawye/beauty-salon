9d9a8b3aa514adedc7dd9ba0d99761293bb916b4
My Name <kaichybekovaayana@gmail.com>
2023-08-27 19:31:15 +0600

    reckeck

--- src/components/RecordPage/ServiceList.tsx
+++ src/components/RecordPage/ServiceList.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import s from "./RecordingPage.module.scss";
 import { useAppDispatch, useAppSelector } from "@/store/store";
 import { createRecordSlice } from "@/store/reducers/createRecordSlice";
